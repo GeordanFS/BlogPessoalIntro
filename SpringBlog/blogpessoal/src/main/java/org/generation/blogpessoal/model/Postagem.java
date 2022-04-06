6775ced88f611edb9c93d7feda5284728343a911
GeordanFS <geordansantos@gmail.com>
2022-04-06 09:36:43 -0300

    Adição de security no blog pessoal

--- SpringBlog/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ SpringBlog/blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -36,6 +36,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 
 	public Long getId() {
@@ -76,5 +80,14 @@ public class Postagem {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+
+}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 }
